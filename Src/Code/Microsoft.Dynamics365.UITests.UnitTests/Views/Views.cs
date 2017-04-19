8fb886c489c00cd87af4d795e2b0e917e45149bd
Uthra Sridhar <v-utsrid@microsoft.com>
2017-04-19 09:59:52 -0700

    Test methods for Options and Views
    Related Work Items: #504630, #504631, #504632, #504633, #504634, #504635, #504636, #504641, #504642, #504643

---
+++ Src/Code/Microsoft.Dynamics365.UITests.UnitTests/Views/Views.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UITests.Api;
+using Microsoft.Dynamics365.UITests.Browser;
+using System;
+using System.Security;
+using System.Threading;
+using OpenQA.Selenium.Support.Events;
+
+namespace Microsoft.Dynamics365.UITests.UnitTests
+{
+    [TestClass]
+    public class Views
+    {
+
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void TestSwitchView()
+        {
+            using (var xrmBrowser = new XrmBrowser(new BrowserOptions
+            {
+                BrowserType = BrowserType.Chrome,
+                PrivateMode = true,
+                FireEvents = true
+            }))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+                xrmBrowser.Grid.SwitchView("Active Accounts");
+
+            }
+        }
+
+
+        [TestMethod]
+        public void TestGetGridItems()
+        {
+            using (var xrmBrowser = new XrmBrowser(new BrowserOptions
+            {
+                BrowserType = BrowserType.Chrome,
+                PrivateMode = true,
+                FireEvents = true
+            }))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+                xrmBrowser.Grid.SwitchView("Active Accounts");
+                xrmBrowser.Grid.GetGridItems();
+
+            }
+        }
+
+        [TestMethod]
+        public void TestOpenGridRecord()
+        {
+            using (var xrmBrowser = new XrmBrowser(new BrowserOptions
+            {
+                BrowserType = BrowserType.Chrome,
+                PrivateMode = true,
+                FireEvents = true
+            }))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+                xrmBrowser.Grid.SwitchView("Active Accounts");
+                xrmBrowser.Grid.OpenGridRecord(0);
+
+            }
+        }
+
+        //[TestMethod]
+        //public void TestSortGridRow()
+        //{
+        //    using (var xrmBrowser = new XrmBrowser(new BrowserOptions
+        //    {
+        //        BrowserType = BrowserType.Chrome,
+        //        PrivateMode = true,
+        //        FireEvents = true
+        //    }))
+        //    {
+        //        xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+        //        xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+        //        xrmBrowser.Grid.SwitchView("Active Accounts");
+        //        xrmBrowser.Grid.Sort("Account Name");
+        //        Thread.Sleep(10000);
+
+        //    }
+        //}
+    }
+}
