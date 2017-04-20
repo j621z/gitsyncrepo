9a64e227fcf4fd5eae03932a3f8904e4084c0bf9
Uthra Sridhar <v-utsrid@microsoft.com>
2017-04-20 12:09:54 -0700

    Test API'S and Bug Fix

---
+++ Src/Code/Microsoft.Dynamics365.UITests.UnitTests/Entity/Entity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UITests.Api;
+using Microsoft.Dynamics365.UITests.Browser;
+using System;
+using System.Security;
+using System.Threading;
+using OpenQA.Selenium.Support.Events;
+
+namespace Microsoft.Dynamics365.UITests.UnitTests
+{
+    [TestClass]
+    public class Entity
+    {
+
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void TestCollapseTab()
+        {
+            using (var xrmBrowser = new XrmBrowser(new BrowserOptions
+            {
+                BrowserType = BrowserType.Chrome,
+                PrivateMode = true,
+                FireEvents = true
+            }))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+                xrmBrowser.Grid.SwitchView("Active Accounts");
+                xrmBrowser.Grid.OpenRecord(0);
+                xrmBrowser.Entity.SelectTab("Summary");
+                Thread.Sleep(1000);
+
+            }
+        }
+
+    }
+}
