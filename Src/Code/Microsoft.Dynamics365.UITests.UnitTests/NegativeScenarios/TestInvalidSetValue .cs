d7bca9789e23083939df2c1bb5d34f908a27501a
Uthra Sridhar <v-utsrid@microsoft.com>
2017-05-12 12:14:35 -0700

    Capture negative scenarios and handle the exceptions for invalid input or unexpected user behavior

---
+++ Src/Code/Microsoft.Dynamics365.UITests.UnitTests/NegativeScenarios/TestInvalidSetValue .cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UITests.Api;
+using Microsoft.Dynamics365.UITests.Browser;
+using System;
+using System.Security;
+using System.Threading;
+using OpenQA.Selenium.Support.Events;
+
+namespace Microsoft.Dynamics365.UITests.UnitTests
+{
+    [TestClass]
+    public class TestInvalidSetValue
+    {
+
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void InvalidSetValue()
+        {
+            using (var xrmBrowser = new XrmBrowser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Leads");
+
+                xrmBrowser.CommandBar.ClickCommand("New");
+
+                xrmBrowser.Entity.SetValue("subject1", "Test API to Create leads");
+
+            }
+        }
+
+        [TestMethod]
+        public void InvalidOptionSetValue()
+        {
+            using (var xrmBrowser = new XrmBrowser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                Thread.Sleep(500);
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Leads");
+
+                xrmBrowser.CommandBar.ClickCommand("New");
+
+                xrmBrowser.Entity.SetValue(new OptionSet { Name = "Invalidcontactmethodcode", Value = "Email" });
+
+            }
+        }
+
+        [TestMethod]
+        public void InvalidOpenLookupSetValue()
+        {
+            using (var xrmBrowser = new XrmBrowser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                Thread.Sleep(500);
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+
+                xrmBrowser.CommandBar.ClickCommand("New");
+
+                xrmBrowser.Entity.SetValue(new Lookup { Name = "prrimarycontactid", Value = "Rene Valdes (sample)" });
+
+            }
+        }
+
+    }
+}
---
+++ Src/Code/Microsoft.Dynamics365.UITests.UnitTests/NegativeScenarios/TestInvalidSwitchView.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Dynamics365.UITests.Api;
+using Microsoft.Dynamics365.UITests.Browser;
+using System;
+using System.Security;
+using System.Threading;
+using OpenQA.Selenium.Support.Events;
+
+namespace Microsoft.Dynamics365.UITests.UnitTests
+{
+    [TestClass]
+    public class TestInvalidSwitchView
+    {
+
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void InvalidSwitchView()
+        {
+            using (var xrmBrowser = new XrmBrowser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                Thread.Sleep(100);
+
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+                xrmBrowser.Grid.SwitchView("Open Accounts");
+
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
