c0a9ae3fb2c058c4624fe5ca196c62d232e4072b
Austin Jones <austinj@microsoft.com>
2017-05-31 19:08:12 +0000

    Merged PR 103174: setup unit test configuration and signing keys
    setup unit test configuration
    setup delay signing on published binaries
    
    Related Work Items: #630545, #630550
    
    Related work items: #630545, #630550

---
+++ Build/Unregister272_SN-VuDelaySignBypass.bat
@@ -0,0 +1,4 @@
+@echo on
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe" -Vu *,f300afd708cefcd3
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\sn.exe" -Vl
+pause
--- Src/Code/Microsoft.Dynamics365.QualityTools.DataPopulation/Properties/AssemblyInfo.cs
+++ Src/Code/Microsoft.Dynamics365.QualityTools.DataPopulation/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.Dynamics365.QualityTools.DataPopulation")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6dcb672d-15a9-474b-b6fa-51bb939dfc14")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
