d355bccd1e3e08f647394f115c612689ebe256c1
Uthra Sridhar <v-utsrid@microsoft.com>
2017-06-09 16:18:45 -0700

    Handle Exception and Create Invalid Test For RelatedGridPage

---
+++ Src/Code/Microsoft.Dynamics365.UIAutomation.UnitTests.Sample/NegativeScenarios/RelatedGrid/InvalidRelatedSwitchView.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security;
+using Microsoft.Dynamics365.UIAutomation.UnitTests;
+
+namespace Microsoft.Dynamics365.UIAutomation.UnitTests.Sample.NegativeScenarios.RelatedGrid
+{
+    [TestClass]
+    public class InvalidRelatedSwitchView
+    {
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+        private readonly Uri _xrmUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["OnlineCrmUrl"].ToString());
+
+        [TestMethod]
+        public void TestInvalidRelatedSwitchView()
+        {
+            using (var xrmBrowser = new XrmBrowser(TestSettings.Options))
+            {
+                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
+
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                xrmBrowser.ThinkTime(500);
+                xrmBrowser.Navigation.OpenSubArea("Sales", "Accounts");
+
+                xrmBrowser.ThinkTime(3000);
+                xrmBrowser.Grid.OpenRecord(0);
+                xrmBrowser.Navigation.OpenRelated("Cases");
+                xrmBrowser.Related.Sort("createdon");
+
+                xrmBrowser.Related.SwitchView("Open Cases");
+                xrmBrowser.ThinkTime(2000);
+            }
+        }
+    }
+}
